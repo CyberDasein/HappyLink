716e0dc00a2f40562b3d4405a91b2fc798cb4b20
Alexey <petrenko012345@yandex.ru>
2025-12-08 21:19:36 +0500

    доработки

--- src/js/templates/retrowave.js
+++ src/js/templates/retrowave.js
@@ -1,4 +1,6 @@
-export default function initRetrowave() {
+export default function initRetrowave(cardData) {
+
+  const { name, message, photo, dateStr } = cardData;
   // Stars Position
   let topp = document.getElementById("top");
   function setStars(numS) {
@@ -23,4 +25,30 @@ export default function initRetrowave() {
     }
   }
   synthSun(8);
+
+  const birthDate = new Date(dateStr);
+  const age = calculateAge(birthDate);
+  // Форматируем дату: "14 АПРЕЛЯ 2025"
+  const formattedDate = formatDate(birthDate);
+
+  document.querySelector('#birth-year').textContent = `${age}`;
+  document.querySelector('#date-str').textContent = `${formattedDate}`;
+}
+
+function formatDate(date) {
+  const day = date.getDate();
+  const month = date.toLocaleString('en-US', { month: 'short' }).toUpperCase();
+  const year = date.getFullYear();
+  const shortYear = year % 100;
+  return `${day} ${month} ${shortYear}`;
+}
+
+function calculateAge(birthDate) {
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
 }
